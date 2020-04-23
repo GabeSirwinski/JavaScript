39f3196e03f887085faf2ac9710da1fa4dd1dfdd
GabeSirwinski <gabrielczerwinski@live.com>
2020-04-23 16:28:55 -0700

    Assignment 58 JS Course

---
+++ JS/JavaScript_File.js
@@ -0,0 +1,34 @@
+window.alert("Hello");
+
+document.write("HEY WHAT'S UP ");
+
+var a = "This is a variable ";
+
+document.write(a);
+
+var b = "This is another variable ";
+
+document.write(b);
+
+document.write("\"I don't want to go\", the girl said. ")
+
+document.write("I am having" + " " + "a great time.");
+
+var c = "Yeet" + " " + "Yaw ";
+
+document.write(c);
+
+var fruits = "food", garbage = "old food", plastic = "not food", iceCream = "yummy food";
+
+document.write("Ice cream is " + iceCream);
+
+var notFood = garbage.fontcolor("red") ;
+var oldFood = plastic.fontcolor("red");
+var yummyFood = iceCream.fontcolor("blue");
+var food = fruits.fontcolor("orange");
+
+document.write(notFood + " " + oldFood + " " + yummyFood + " " + food);
+
+var d = 5 + 5;
+
+document.write(d);
